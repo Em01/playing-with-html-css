2271b7f409eb17edb221be67e60d7e7d2eaf9387
Emma <emma_williams1@hotmail.co.uk>
2014-04-27 14:57:30 +0100

    some html files and an image using sinatra

---
+++ Gemfile
@@ -0,0 +1,4 @@
+# this is the gemfile
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'shotgun'
---
+++ hello.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+ get '/' do
+  @name = %w(Amigo Oscar Viking).sample
+  erb :index
+end
+
+get '/hello' do 
+  @visitor = params[:name]
+  erb :index
+end
+
+#   @name = %w(Amigo Oscar Viking).sample
+#   #@name selects a name for the kitten in the instance variable @name
+#   erb :index
+# end
+
+# 'hello!'
+# end
+
+# get '/secret' do 
+#   'This is a secret page'
+# end
+
+
+
